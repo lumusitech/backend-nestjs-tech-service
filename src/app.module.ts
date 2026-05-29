3319df12f675c40bd70874943e66af7d7346f39f
Figueroa, Carlos Luciano <90582206+lumusitech@users.noreply.github.com>
2026-05-29 20:25:33 -0300

    chore: add base project config with Docker, TypeORM, validation (#7)

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,9 +1,27 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule, ConfigService } from '@nestjs/config';
+import { TypeOrmModule } from '@nestjs/typeorm';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  imports: [
+    ConfigModule.forRoot({ isGlobal: true }),
+    TypeOrmModule.forRootAsync({
+      inject: [ConfigService],
+      useFactory: (configService: ConfigService) => ({
+        type: 'postgres',
+        host: configService.get('DB_HOST'),
+        port: configService.get('DB_PORT'),
+        username: configService.get('DB_USERNAME'),
+        password: configService.get('DB_PASSWORD'),
+        database: configService.get('DB_NAME'),
+        entities: [__dirname + '/**/*.entity{.ts,.js}'],
+        synchronize: configService.get('NODE_ENV') !== 'production', // Solo true en desarrollo
+        logging: configService.get('DB_LOGGING') === 'true',
+      }),
+    }),
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
