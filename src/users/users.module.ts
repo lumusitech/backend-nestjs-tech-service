98070a06e3d7f58b6bbb0b6431cd84cc39782256
Luciano Figueroa <lumusitech@gmail.com>
2026-06-26 18:44:43 -0300

    fix: register SkillsModule in UsersModule to resolve SkillRepository DI

--- src/users/users.module.ts
+++ src/users/users.module.ts
@@ -3,9 +3,10 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { User } from './entities/user.entity';
 import { UsersService } from './users.service';
 import { UsersController } from './users.controller';
+import { SkillsModule } from '../skills/skills.module';
 
 @Module({
-  imports: [TypeOrmModule.forFeature([User])],
+  imports: [TypeOrmModule.forFeature([User]), SkillsModule],
   controllers: [UsersController],
   providers: [UsersService],
   exports: [UsersService],
